91f0935049cd5998099bf2a78a4439828eda0549
root <fajarhide@gmail.com>
2015-01-26 11:12:01 +0700

    Installer v.1.0

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ -d '/usr/local/search' ]; then
+	echo; echo; echo "Please uninstall the previous version first!"
+	exit 0
+else
+	mkdir /usr/local/search
+fi
+clear
+
+echo; echo 'Installing search 1.0'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/search/README https://raw.githubusercontent.com/fajarhide/search/master/README.md
+echo -n '.'
+wget -q -O /usr/local/search/search.pl https://raw.githubusercontent.com/fajarhide/search/master/search.pl
+chmod 0755 /usr/local/search/search.pl
+cp -s /usr/local/search/search.pl /usr/local/sbin/search
+echo '... Done!'
+cat /usr/local/search/README | less
